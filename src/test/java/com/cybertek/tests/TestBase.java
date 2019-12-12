02d8484e25b7151f8db2bfdb2f4d802e332d9767
Umit Uzulmez <umituzulmez@gmail.com>
2019-12-12 21:42:04 +0000

    day13

---
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -0,0 +1,33 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    protected WebDriver driver; // public works too
+    protected Actions action;
+    protected WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = Driver.get();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        action = new Actions(driver);
+        wait = new WebDriverWait(driver,10);
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+}
--- src/test/java/com/cybertek/tests/day13_properties_driver_testbase/SingletonTest.java
+++ src/test/java/com/cybertek/tests/day13_properties_driver_testbase/SingletonTest.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day13_properties_driver_testbase;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SingletonTest {
@@ -16,6 +19,23 @@ public class SingletonTest {
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
 
+    }
+
+    @Test
+    public void test2(){
+        //this is how we create webdriver object from now on
+        //type of the driver will come from configuration.properties file
+        WebDriver driver = Driver.get();
+        //we are getting url from configuration.properties file
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().navigate().refresh();
+    }
+    @Test
+    public void test3() throws InterruptedException {
+        Thread.sleep(2000);
+        Driver.get().get("http://amazon.com");
+        //if we are using Driver close, we use closeDriver method to close the driver.
+        Driver.closeDriver();
     }
 
 
